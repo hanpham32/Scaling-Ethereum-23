60e3a17cd9bc2c59d974d993749c406ff8305650
kimlong0 <kimlong0@uw.edu>
2023-03-20 16:04:57 -0700

    hover adjustments

--- familysharing-frontend/src/pages/onboarding.tsx
+++ familysharing-frontend/src/pages/onboarding.tsx
@@ -7,36 +7,36 @@ function Onboarding() {
                 <div className='px-6 py-32'>
                     <div>
                         <h1 className="text-5xl font-bold text-[#1B2C5D] mb-4">Welcome to FamilyShare</h1>
-                        <p className="text-2xl ">A family centered multisignature wallet utilizing account abstraction.</p>
+                        <p className="text-xl ">A family centered multisignature wallet utilizing account abstraction.</p>
                     </div>
                     <div className="flex flex-col items-center lg:flex-row gap-8 mt-16">
-                        <div className="h-96 w-[600px] bg-white rounded-xl p-8">
+                        <div className="h-80 w-[560px] bg-white rounded-xl p-8">
                             <Image 
                                 src='/plusIcon.svg'
                                 alt='Add Icon'
                                 width={60}
                                 height={60}
                             />
-                            <div className="mt-12 mb-6">
+                            <div className="mt-10 mb-6">
                                 <h2 className="text-2xl font-bold text-[#1B2C5D]">Create a FamilyShare</h2>
                                 <p className="text-[#1B2C5D]">A new Safe that is controlled by one or multiple owners.</p>
                             </div>
-                            <button className="h-12 w-64 bg-[#1B2C5D] font-semibold rounded-xl text-lg text-white">
+                            <button className="h-12 w-64 bg-[#1B2C5D] font-semibold rounded-xl text-lg text-white hover:opacity-80 ease-in duration-200">
                                 Create new FamilyShare
                             </button>
                         </div>
-                        <div className="h-96 w-[600px] bg-white rounded-xl p-8">
+                        <div className="h-80 w-[560px] bg-white rounded-xl p-8">
                             <Image 
                                 src='/northEastArrowIcon.svg'
                                 alt='Arrow Icon'
                                 width={60}
                                 height={60}
                             />
-                            <div className="mt-12 mb-6">
+                            <div className="mt-10 mb-6">
                                 <h2 className="text-2xl font-bold text-[#1B2C5D]">Add existing FamilyShare</h2>
                                 <p className="text-[#1B2C5D]">Already have a Safe? Add your Safe using your Safe address.</p>
                             </div>
-                            <button className="h-12 w-64 border-[#1B2C5D] border-2 font-semibold rounded-xl text-lg text-[#1B2C5D]">
+                            <button className="h-12 w-64 border-[#1B2C5D] border-2 font-semibold rounded-xl text-lg text-[#1B2C5D] hover:bg-slate-200 ease-in duration-200">
                                 Add existing FamilyShare
                             </button>
                         </div>
