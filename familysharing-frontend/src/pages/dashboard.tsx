e23c953f795a68a542fb674b1a5f2e030209c745
kimlong0 <kimlong0@uw.edu>
2023-04-17 20:34:15 -0700

    add balance, message, and funtions cards starter

--- familysharing-frontend/src/pages/dashboard.tsx
+++ familysharing-frontend/src/pages/dashboard.tsx
@@ -3,12 +3,12 @@ import Image from 'next/image';
 
 function Dashboard() {
     return (
-        <div className='flex'>
+        <div className='flex text-primary-color'>
             {/* <Sidebar /> */}
             <div className='full-dashboard'>
                 <div className='left-dashboard flex flex-col justify-center items-center max-w-7xl m-auto'>
                     <div className='px-6 py-16'>
-                        <div className='flex items-center'>
+                        <div className='flex items-center mb-4'>
                             <Image 
                                 src='icons/person1Icon.svg'
                                 width={50}
@@ -16,15 +16,41 @@ function Dashboard() {
                                 alt='profile image'
                             />
                             <div className='ml-4'>
-                                <h2 className='text-3xl md:text-4xl font-bold text-primary-color mb-2'>Michael Chan</h2>
+                                <h2 className='text-3xl md:text-4xl font-bold mb-2'>Michael Chan</h2>
                                 <p>Parent</p>
                             </div>
                         </div>
                         
-                        <div className='wallet-functions'>
-                            <div>
-                                <h3>Your Balance</h3>
-                                <p>$33,200.95</p>
+                        <div className='wallet-functions flex gap-4'>
+                            <div className='bg-[#F0FCFF] rounded-3xl p-6'>
+                                <h3 className='text-3xl mb-4'>Your Balance</h3>
+                                <p className='text-3xl font-bold mb-4'>$33,200.95</p>
+                                <Image 
+                                    src='icons/coinsIcon.svg'
+                                    alt='coins'
+                                    width={99}
+                                    height={80}
+                                />
+                            </div>
+                            <div className='bg-[#F0FCFF] rounded-3xl p-6'>
+                                <Image 
+                                    src='icons/messageIcon.svg'
+                                    alt='coins'
+                                    width={99}
+                                    height={80}
+                                />
+                                <h3 className='text-3xl mb-4 font-bold'>New Message!</h3>
+                            </div>
+                            <div className='flex flex-col gap-2'>
+                                <button className="py-2 px-6 bg-primary-color font-semibold rounded-xl text-lg text-white hover:opacity-80 hover:scale-110 ease-in duration-200">
+                                    Send
+                                </button>
+                                <button className="py-2 px-4 bg-primary-color font-semibold rounded-xl text-lg text-white hover:opacity-80 hover:scale-110 ease-in duration-200">
+                                    Receive
+                                </button>
+                                <button className="py-2 px-4 bg-primary-color font-semibold rounded-xl text-lg text-white hover:opacity-80 hover:scale-110 ease-in duration-200">
+                                    Buy
+                                </button>
                             </div>
                         </div>
                     </div>
